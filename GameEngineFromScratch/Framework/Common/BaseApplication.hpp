51421c739998442511d2a6cb36cb98647e01d60f
Ralph <kingiii003@gmail.com>
2018-02-27 21:19:53 +0800

    Lesson7 Commit

--- GameEngineFromScratch/Framework/Common/BaseApplication.hpp
+++ GameEngineFromScratch/Framework/Common/BaseApplication.hpp
@@ -6,6 +6,13 @@ namespace My
 {
 	class BaseApplication : implements IApplication
 	{
-		
+	public:
+		virtual int Initialize();
+		virtual void Finalize();
+		virtual void Tick();
+		virtual bool IsQuit();
+
+	protected:
+		bool m_bQuit;
 	};
 }
